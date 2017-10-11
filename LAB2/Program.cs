85936de4966ad83570491aa88894b6b059cdf5de
Ronald Microtech <Ronald Microtech>
2017-10-11 12:19:39 +0300

    Add project files.

---
+++ LAB2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.Height = 10;
+            r.Width = 5;
+            r.Print();
+
+            Square s = new Square(5);
+            s.Print();
+
+            Circle c = new Circle(8);
+            c.Print();
+            Console.ReadLine();
+
+        }
+    }
+}
+
